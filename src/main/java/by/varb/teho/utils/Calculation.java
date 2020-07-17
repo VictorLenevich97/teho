788d5fbedc08be194fc72c22c354a912fb663e99
Владислав Лычков <lychkov@RIT-L014.rit.va>
2020-07-17 11:38:22 +0300

    Табуляция

--- src/main/java/by/varb/teho/utils/Calculation.java
+++ src/main/java/by/varb/teho/utils/Calculation.java
@@ -2,16 +2,16 @@ package by.varb.teho.utils;
 
 public final class Calculation {
 	
-    //расчёт предполагаемого выхода из строя групп ВВСТ
+	//расчёт предполагаемого выхода из строя групп ВВСТ
     public static double [] calculateEquipmentFailure(int [] groups, double [] dailyAverageLoss, double kM) {
   		double[] wJ = new double [groups.length];
   		for (int i = 0; i < groups.length; i++) {
   			wJ[i] = (groups[i] * dailyAverageLoss[i]) / 100 * kM;
   		}
   		return wJ;
-  	}
+    }
   	
-  	//вычисление количества образцов ВВСТ, требующих ремонта(текущий и средний)
+    //вычисление количества образцов ВВСТ, требующих ремонта(текущий и средний)
     public static double [][] сalculateEquipmentRequiringRepair(int[] qMinArray, int[] qMaxArray, double [] wJ, int [] qjMax) {
   		double[][] wjArray = new double[wJ.length][qMinArray.length];
           for (int i = 0; i < wJ.length; i++) 
@@ -20,9 +20,9 @@ public final class Calculation {
   	        	wjArray[i][j] = Math.abs((wJ[i] * (Math.sin((Math.PI * qMaxArray[j]) / (2 * qjMax[i])) - Math.sin((Math.PI * qMinArray[j]) / (2 * qjMax[i])))));
   	        }
           return wjArray;
-  	}
+    }
   	
-  	//расчёт средней трудоёмкости ремонта ВВСТ
+    //расчёт средней трудоёмкости ремонта ВВСТ
     public static double [][] calculateAverageComplexityRepair(double [][] wjArray, int[] qMaxArray) {
   		double[][] qjArray = new double[wjArray.length][wjArray[0].length];
   		for (int i = 0; i < qjArray.length; i++) 
