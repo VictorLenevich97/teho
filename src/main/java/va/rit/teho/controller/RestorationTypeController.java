7dcb0ab5ef64d93a63b66ac03d6b34d816de4c84
Александр Барановский <baranovskiy@rit.va>
2021-01-20 17:16:47 +0300

    Добавлен эндпоинт для получения списка типов восстановления

---
+++ src/main/java/va/rit/teho/controller/RestorationTypeController.java
@@ -0,0 +1,38 @@
+package va.rit.teho.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import va.rit.teho.dto.common.IdAndNameDTO;
+import va.rit.teho.entity.labordistribution.RestorationType;
+import va.rit.teho.service.labordistribution.RestorationTypeService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping(path = "/restoration-type", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestorationTypeController {
+
+    private final RestorationTypeService restorationTypeService;
+
+    public RestorationTypeController(RestorationTypeService restorationTypeService) {
+        this.restorationTypeService = restorationTypeService;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Получить список типов восстановления")
+    @ResponseBody
+    public ResponseEntity<List<IdAndNameDTO>> listRepairTypes() {
+        List<RestorationType> restorationTypes = restorationTypeService.list();
+        return ResponseEntity.ok(restorationTypes
+                                         .stream()
+                                         .map(rt -> new IdAndNameDTO(rt.getId(), rt.getName()))
+                                         .collect(Collectors.toList()));
+    }
+
+}
