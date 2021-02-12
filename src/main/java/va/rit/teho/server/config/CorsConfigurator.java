1fa7d9431b5639f6b82ec0ec3d58ccdb64d8c6d2
Александр Барановский <baranovskiy@rit.va>
2021-02-12 16:39:36 +0300

    #1408: dev/production профили (отключение swagger'a, включение cors только для UI)

--- src/main/java/va/rit/teho/server/config/CorsConfigurator.java
+++ src/main/java/va/rit/teho/server/config/CorsConfigurator.java
@@ -1,35 +1,41 @@
 package va.rit.teho.server.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
+import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfigurator {
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
-        config.setAllowedHeaders(Arrays.asList("X-Requested-With",
-                                               "Origin",
-                                               "Content-Type",
-                                               "Accept",
-                                               "Authorization",
-                                               "Session-Id"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
+    @Profile("production")
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:60140")
+                        .allowedHeaders("Session-Id");
+            }
+        };
+    }
+
+    @Bean
+    @Profile("dev")
+    public WebMvcConfigurer corsConfigurerDev() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("Session-Id");
+            }
+        };
     }
 
 }
