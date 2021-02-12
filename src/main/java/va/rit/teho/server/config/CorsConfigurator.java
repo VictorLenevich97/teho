27ca1dc66c9d47ece4d1386bf92e096cfb52cb24
Александр Барановский <baranovskiy@rit.va>
2021-02-12 17:23:47 +0300

    #1408: старый способ конфигурации CORS

--- src/main/java/va/rit/teho/server/config/CorsConfigurator.java
+++ src/main/java/va/rit/teho/server/config/CorsConfigurator.java
@@ -1,11 +1,18 @@
 package va.rit.teho.server.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
 import org.springframework.lang.NonNull;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfigurator {
@@ -26,16 +33,22 @@ public class CorsConfigurator {
 
     @Bean
     @Profile("dev")
-    public WebMvcConfigurer corsConfigurerDev() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedHeaders("Session-Id");
-            }
-        };
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
+        config.setAllowedHeaders(Arrays.asList("X-Requested-With",
+                                               "Origin",
+                                               "Content-Type",
+                                               "Accept",
+                                               "Authorization",
+                                               "Session-Id"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
 }
