977a51cb2e0298b012dfc8578b40417903cb8315
baranovskiy <baranovskiy@rit.va>
2021-05-15 16:22:26 +0300

    TEHO-1569: убрал Cookie, изменил код результата в случае неверной сесии (теперь 412)

--- src/main/java/va/rit/teho/server/config/SessionFilter.java
+++ src/main/java/va/rit/teho/server/config/SessionFilter.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 import va.rit.teho.entity.session.TehoSession;
 import va.rit.teho.exception.NotFoundException;
 import va.rit.teho.service.session.SessionService;
@@ -12,12 +11,9 @@ import va.rit.teho.service.session.SessionService;
 import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -37,32 +33,27 @@ public class SessionFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse httpServletResponse,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String sessionId = httpServletRequest.getHeader("Session-Id");
-        TehoSession session;
-        Cookie cookie = WebUtils.getCookie(httpServletRequest, "TEHO_SESSION_ID");
-        if(cookie != null) {
-            session = sessionService.get(UUID.fromString(cookie.getValue()));
-            tehoSession.saveSession(session);
-        } else if (sessionId == null || sessionId.equals("null")) {
-            //prepareBadRequestResponse(httpServletResponse, "Missing Session-Id header!");
+        if (sessionId == null || sessionId.equals("null")) {
+            prepareSessionErrorResponse(httpServletResponse, "Missing Session-Id header!");
         } else {
             UUID sessionUUID = UUID.fromString(sessionId);
+            TehoSession session;
             try {
                 session = sessionService.get(sessionUUID);
-                Cookie newCookie = new Cookie("TEHO_SESSION_ID", sessionId);
-                httpServletResponse.addCookie(newCookie);
-                tehoSession.saveSession(session);
             } catch (NotFoundException e) {
-                prepareBadRequestResponse(httpServletResponse, "Session \"" + sessionUUID + "\" not found!");
+                prepareSessionErrorResponse(httpServletResponse, "Session \"" + sessionUUID + "\" not found!");
                 return;
             }
+            tehoSession.saveSession(session);
+
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
         }
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
-    private void prepareBadRequestResponse(HttpServletResponse httpServletResponse, String message) throws
+    private void prepareSessionErrorResponse(HttpServletResponse httpServletResponse, String message) throws
             IOException {
         httpServletResponse.resetBuffer();
-        httpServletResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        httpServletResponse.setStatus(HttpStatus.PRECONDITION_FAILED.value());
         httpServletResponse.setHeader("Content-Type", "application/json");
         httpServletResponse.getOutputStream().print("{\"message\": \"" + message + "\"}");
         httpServletResponse.flushBuffer();
